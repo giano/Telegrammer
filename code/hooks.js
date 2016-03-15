48f28e6fee8734103a4ac5a61e95a0d9710b96f9
Stefano Valicchia <stefano.valicchia@gmail.com>
2016-03-15 15:57:55 +0100

    First release

--- code/hooks.js
+++ code/hooks.js
@@ -2,31 +2,130 @@
 
 const config = require('./config');
 const Promise = require('promise');
+const escape_string_regexp = require('escape-string-regexp');
+const _ = require('underscore');
 
-module.exports = {
+const hooks = {
   load: function () {
     let promise = new Promise(function (resolve, reject) {
       const path = require('path');
       const dir = path.resolve(__dirname, '..');
-      const hooks_dir = path.resolve(dir, config.get("hooks:folder"));
-      const dotfiles = require('glob-fs-dotfiles');
-      const glob = require('glob-fs')({
-          cwd: hooks_dir,
-          root: hooks_dir
-        })
-        .exclude('node_modules')
-        .use(dotfiles());
-      const hooks_pattern = `**/*.(js|json|coffee)`;
-      glob.readdirPromise(hooks_pattern).then(function (matches) {
-        console.log(matches);
+      const hooks_dir = path.resolve(dir, config.get('hooks:folder'));
+      const glob = require("glob");
+      const options = {
+        cwd: hooks_dir,
+        root: hooks_dir
+      };
+      const hooks_pattern = `**/*.{js,json,coffee}`;
+      glob(hooks_pattern, options, function (error, matches) {
+        if (error) return reject(error);
         matches = matches || [];
-        let matches_out = [];
+        let matches_out = matches.map(function (hook_path) {
+          let hook_complete_path = path.resolve(hooks_dir, hook_path);
+          let hook_def = require(hook_complete_path);
+          return work_hook(hook_def, hook_path);
+        });
+        matches_out = matches_out.filter(function (e) {
+          return e
+        });
         resolve(matches_out);
-      }).catch(function (err) {
-        reject(err);
       });
     });
     return promise;
-
   }
-}
+};
+
+const work_hook = function (hook_def, hook_path) {
+  if (hook_def) {
+    if (((hook_def.match || hook_def.command) && hook_def.action) || (hook_def.hook)) {
+      const path = require('path');
+      hook_def.namespace = hook_def.namespace || path.dirname(hook_path) || 'default';
+      hook_def.name = hook_def.name || path.basename(hook_path, path.extname(hook_path));
+      hook_def.full_name = `${hook_def.namespace}/${hook_def.name}`;
+      hook_def.has_telegram_hook = hook_def.action !== null;
+      hook_def.has_page_hook = hook_def.hook !== null;
+
+      if (hook_def.match && _.isString(hook_def.match)) {
+        hook_def.match = new RegExp(escape_string_regexp(hook_def.match), "igm");
+      }
+
+      if (hook_def.command) {
+        if (hook_def.command.indexOf('/') == -1) {
+          hook_def.command = `/${hook_def.command}`;
+        }
+        hook_def.command = hook_def.command.toLowerCase();
+      }
+
+      if (_.isString(hook_def.action)) {
+        let action_command = hook_def.action;
+        let action_fn = _.bind(function (message, service, matches, cb) {
+          const exec = require('child_process').exec;
+          matches = matches || [];
+          let result_command = action_command;
+          for (let i = 0; i < matches.length; i++) {
+            let placeholder = new RegExp(`@${i}@`, 'mgi');
+            result_command = result_command.replace(placeholder, matches[i]);
+          }
+          let child = exec(result_command, function (error, stdout, stderr) {
+            let error_msg = hook_def.error || "Error: @error@";
+            let has_error = false;
+
+            if (error) return cb(error, null);
+
+            let stderr_str = stderr.toString('utf8');
+            let stdout_str = stdout.toString('utf8');
+
+            if (stderr_str) return cb(stderr_str, null);
+
+            if (hook_def.check) {
+              if (_.isString(hook_def.check)) {
+                has_error = hook_def.check.toLowerCase() != stdout_str.toLowerCase();
+              } else if (_.isFunction(hook_def.check)) {
+                has_error = !hook_def.check(message, stdout_str);
+              } else if (_.isRegExp(hook_def.check)) {
+                has_error = !hook_def.check.test(stdout_str);
+              }
+            }
+
+            if (has_error) {
+              cb(stdout_str, null);
+            } else if (_.isString(hook_def.response)) {
+              cb(stdout_str, stdout_str);
+            }
+
+          });
+        }, hook_def);
+
+        hook_def.action = action_fn;
+      }
+
+      if (hook_def.action) {
+        let _action = _.bind(hook_def.action, hook_def);
+
+        hook_def.action = _.bind(function (message, service, matches, cb) {
+          let error_msg = hook_def.error || "Error: @error@";
+          let response_msg = hook_def.response || "@response@";
+
+          _action(message, service, matches, function (error, output) {
+            cb(error, output);
+
+            if (hook_def.response === false) {
+              return;
+            }
+
+            if (error) {
+              return service.respond(message, error_msg.replace(/@error@/mgi, (error.message || error)));
+            } else {
+              output = output || "";
+              return service.respond(message, response_msg.replace(/@response@/mgi, output));
+            }
+          })
+        }, hook_def);
+      }
+      return hook_def;
+    }
+  }
+  return false;
+};
+
+module.exports = hooks;
--- code/telegram.js
+++ code/telegram.js
@@ -1,28 +1,98 @@
 "use strict";
 
 const config = require('./config');
+const _ = require('underscore');
 
 const Promise = require('promise');
 const Telegram = require('telegram-bot-api');
-var api = null;
+const device_regex = /%([\d\w_\-\.]+)/i;
 
-module.exports = {
+let api = null;
+let telegram_hooks = [];
+
+const telegram_service = {
+
+  respond: function (message, content) {
+    api.sendMessage({
+      reply_to_message_id: message.id,
+      text: content,
+      chat_id: message.chat.id
+    });
+  },
+
   init: function (hooks) {
+
+    telegram_hooks = hooks.filter(function (el) {
+      return el.has_telegram_hook
+    });
+
     let promise = new Promise(function (resolve, reject) {
       var token = config.get("telegram:token");
+
       api = new Telegram({
         token: token,
         updates: {
-          enabled: true
+          enabled: true,
+          pooling_timeout: 6000
         }
       });
-      api.getMe().then(function(data) {
-        console.log(data);
+
+      api.getMe().then(function (data) {
+        data = data || {};
         resolve(api, hooks);
       }).catch(function (err) {
         reject(err);
+      });
+
+      api.on('message', function (message) {
+        if (message.from && message.from.username == config.get('device_owner_username')) {
+          let message_text = message.text || message.caption;
+          if (message_text) {
+            let to_device = "all";
+            if (device_regex.test(message_text)) {
+              let to_device = (device_regex.exec(message_text)[1] || "!!!").toString().toLowerCase();
+            }
+            if (!config.get("telegram:chat_id") && message.chat && message.chat.id) {
+              config.set("telegram:chat_id", message.chat.id);
+            }
+            for (let hook_id in telegram_hooks) {
+              let hook = telegram_hooks[hook_id];
+              if (hook.all || config.get('device_name') == to_device || _.contains(config.get('execute_devices'), to_device)) {
+                if (hook.command) {
+                  if (message_text == hook.command) {
+                    console.log(`Executing ${hook.full_name}`);
+                    hook.action(message, telegram_service, [], function (error, response) {
+                      if (error) {
+                        console.error(`Error executing ${hook.full_name}: ${error}`);
+                      } else {
+                        console.log(`Executed ${hook.full_name}`);
+                      }
+                    });
+                    return;
+                  }
+                } else if (hook.match) {
+                  let regex_to_match = hook.match;
+                  if (regex_to_match.test(message_text)) {
+                    console.log(`Executing ${hook.full_name}`);
+                    let matches = regex_to_match.exec(message_text);
+                    hook.action(message, telegram_service, matches, function (error, response) {
+                      if (error) {
+                        console.error(`Error executing ${hook.full_name}: ${error}`);
+                      } else {
+                        console.log(`Executed ${hook.full_name}`);
+                      }
+                    });
+                    return;
+                  }
+                }
+              }
+            }
+          }
+        }
       });
     });
     return promise;
   }
 }
+
+module.exports = telegram_service;
