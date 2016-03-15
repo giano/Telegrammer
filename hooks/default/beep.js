f7e0a40dcea6793edb1c0905dcc5114e199f79b6
Stefano Valicchia <stefano.valicchia@gmail.com>
2016-03-15 20:42:37 +0100

    first release

---
+++ hooks/default/beep.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "match": "beep",
+  "action": function(message, service, matches, cb){
+    console.log('\u0007');
+    cb(null);
+  },
+  "error": "Something wrong happened: @error@",
+  "response": "Ok, beeped!"
+}
