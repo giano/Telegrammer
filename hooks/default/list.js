9d598fe7978c227d9eb53335b80e16bea0ebd146
Stefano Valicchia <stefano.valicchia@gmail.com>
2016-03-17 16:15:01 +0100

    promis'ed everything

--- hooks/default/list.js
+++ hooks/default/list.js
@@ -5,7 +5,7 @@ module.exports = {
   command: "list",
   action: function (message, service, matches) {
 
-    return Promise.resolve(`${config.get("telegram:device_name_char")}${config.get("device_name")} of group ${config.get("telegram:device_name_char")}${config.get("device_group")}`);
+    return Promise.resolve(`*${config.get("telegram:device_name_char")}${config.get("device_name")}* of group *${config.get("telegram:device_name_char")}${config.get("device_group")}*`);
   },
   response: true
 }
