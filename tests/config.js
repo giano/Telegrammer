79907815d247902aee9ff9f3f58282ba20bd8d96
Stefano Valicchia <stefano.valicchia@gmail.com>
2016-04-15 17:23:37 +0200

    written simple config test

---
+++ tests/config.js
@@ -0,0 +1,27 @@
+'use strict';
+const vows = require('vows');
+const chai = require('chai');
+chai.should();
+const assert = chai.assert;
+
+vows.describe('Config').addBatch({
+  'Test config': {
+    topic: function () {
+      process.env.NODE_ENV = 'test';
+      return require('../code/config.js');
+    },
+    'Can be read': function (config) {
+      assert.equal(config.get('leave_me_here_for_tests'), true);
+    },
+    'Allows tree traversing': function (config) {
+      assert.ok(config.get('hooks:folder'));
+    },
+    'Has Telegram Token set': function (config) {
+      assert.ok(config.get('telegram:token'));
+    },
+    'Allows setting': function (config) {
+      config.set('just_a_test', true);
+      assert.ok(config.get('just_a_test'));
+    }
+  }
+}).export(module);
